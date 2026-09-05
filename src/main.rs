3ac0443d31d824ab6fc1be0b62a5d9fe3fd7f5e3
m0nkene <157753157+m0nkene@users.noreply.github.com>
2026-09-05 17:49:44 -0400

    Refactor to implement Calc correctly
    Needed to refactor the code to implement the Calculator structure. The implementation now has the update and view command within the Calculator implementation function. This will allow for settings to be used on the application window

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use iced::widget::{Button, text, Text, Grid, Column, Container, Row, column, container, row};
-use iced::{Alignment, Fill, Element, Theme, Renderer};
+use iced::{Alignment, Fill, Element, Theme, Renderer, Length, FillPortion, Settings, window};
 
 const BUTTON_SIZE: u32 = 50;
 
 //struct that sets the type of data that the object can take. structs are basically arrays/tuples that can be used as immutable datatypes
 #[derive(Default)]
 struct TextStack{
+    value: String,
+}
+
+#[derive(Default)]
+struct Calculator{
     value: String,
 }
 
@@ -16,265 +21,228 @@ enum Message {
     Evaluate,
 }
 
-fn update (stack: &mut TextStack, message: Message){
+
+impl Calculator{
+
+    fn update (&mut self, message: Message){
     match message{
         Message::Input(ValueIn)=>{
-            stack.value.push_str(&ValueIn);
+            self.value.push_str(&ValueIn);
         },
         Message::Evaluate=>todo!(),
-    }
-}
-
-
-fn view (stack: &TextStack) -> Element<'_, Message> {
-
-    let text_box = text::<Theme, Renderer>(stack.value.clone()).size(20);
-
-    let mut grid = Column::new().spacing(10);
-
-    for i in 0..4{
-        let mut row = Row::new().spacing(10);
-
-        for j in 0..4{
-            row = row.push(
-                match (i,j){
-                    (0,0) => {
-                        Button::new(
-                            Container::new(
-                                Text::new("7")
-                            )
-                        )
-                        .width(BUTTON_SIZE)
-                        .height(BUTTON_SIZE)
-                        .on_press(Message::Input("7".to_string()))
-                    },
-                    (0,1) => {
-                        Button::new(
-                            Container::new(
-                                Text::new("8")
-                            )
-                        )
-                        .width(BUTTON_SIZE)
-                        .height(BUTTON_SIZE)
-                        .on_press(Message::Input("8".to_string()))
-                    },
-                    (0,2) => {
-                        Button::new(
-                            Container::new(
-                                Text::new("9")
-                            )
-                        )
-                        .width(BUTTON_SIZE)
-                        .height(BUTTON_SIZE)
-                        .on_press(Message::Input("9".to_string()))
-                    },
-                    (0,3) => {
-                        Button::new(
-                            Container::new(
-                                Text::new("/")
-                            )
-                        )
-                        .width(BUTTON_SIZE)
-                        .height(BUTTON_SIZE)
-                        .on_press(Message::Input("/".to_string()))
-                    },
-                    (1,0) => {
-                        Button::new(
-                            Container::new(
-                                Text::new("4")
-                            )
-                        )
-                        .width(BUTTON_SIZE)
-                        .height(BUTTON_SIZE)
-                        .on_press(Message::Input("4".to_string()))
-                    },
-                    (1,1) => {
-                        Button::new(
-                            Container::new(
-                                Text::new("5")
-                            )
-                        )
-                        .width(BUTTON_SIZE)
-                        .height(BUTTON_SIZE)
-                        .on_press(Message::Input("5".to_string()))
-                    },
-                    (1,2) => {
-                        Button::new(
-                            Container::new(
-                                Text::new("6")
-                            )
-                        )
-                        .width(BUTTON_SIZE)
-                        .height(BUTTON_SIZE)
-                        .on_press(Message::Input("6".to_string()))
-                    },
-                    (1,3) => {
-                        Button::new(
-                            Container::new(
-                                Text::new("*")
-                            )
-                        )
-                        .width(BUTTON_SIZE)
-                        .height(BUTTON_SIZE)
-                        .on_press(Message::Input("*".to_string()))
-                    },
-                    (2,0) => {
-                        Button::new(
-                            Container::new(
-                                Text::new("1")
-                            )
-                        )
-                        .width(BUTTON_SIZE)
-                        .height(BUTTON_SIZE)
-                        .on_press(Message::Input("1".to_string()))
-                    },
-                    (2,1) => {
-                        Button::new(
-                            Container::new(
-                                Text::new("2")
-                            )
-                        )
-                        .width(BUTTON_SIZE)
-                        .height(BUTTON_SIZE)
-                        .on_press(Message::Input("2".to_string()))
-                    },
-                    (2,2) => {
-                        Button::new(
-                            Container::new(
-                                Text::new("3")
-                            )
-                        )
-                        .width(BUTTON_SIZE)
-                        .height(BUTTON_SIZE)
-                        .on_press(Message::Input("3".to_string()))
-                    },
-                    (2,3) => {
-                        Button::new(
-                            Container::new(
-                                Text::new("-")
-                            )
-                        )
-                        .width(BUTTON_SIZE)
-                        .height(BUTTON_SIZE)
-                        .on_press(Message::Input("-".to_string()))
-                    },
-                    (3,0) => {
-                        Button::new(
-                            Container::new(
-                                Text::new("0")
-                            )
-                        )
-                        .width(BUTTON_SIZE)
-                        .height(BUTTON_SIZE)
-                        .on_press(Message::Input("0".to_string()))
-                    },
-                    (3,1) => {
-                        Button::new(
-                            Container::new(
-                                Text::new(".")
-                            )
-                        )
-                        .width(BUTTON_SIZE)
-                        .height(BUTTON_SIZE)
-                        .on_press(Message::Input(".".to_string()))
-                    },
-                    (3,2) => {
-                        Button::new(
-                            Container::new(
-                                Text::new("=")
-                            )
-                        )
-                        .width(BUTTON_SIZE)
-                        .height(BUTTON_SIZE)
-                        .on_press(Message::Evaluate)
-                    },
-                    (3,3) => {
-                        Button::new(
-                            Container::new(
-                                Text::new("+")
-                            )
-                        )
-                        .width(BUTTON_SIZE)
-                        .height(BUTTON_SIZE)
-                        .on_press(Message::Input("+".to_string()))
-                    },
-                    (_,_) => {
-                        Button::new(
-                            Container::new(
-                                Text::new("A")
-                            )
-                        )
-                    }
-                }
-            )
-
         }
-        grid = grid.push(row);
     }
 
-    Column::new()
-        .push(text_box)
-        .push(grid)
-    .into()
+    fn view (&self) -> Element<'_, Message> {
+
+        let text_box = text::<Theme, Renderer>(self.value.clone()).size(40).height(Length::Fill).width(Length::Fill);
+
+        let mut grid = Column::new().spacing(10).height(Length::FillPortion(2)).width(Length::Fill);
+
+        for i in 0..4{
+            let mut row = Row::new().spacing(10);
+
+            for j in 0..4{
+                row = row.push(
+                    match (i,j){
+                        (0,0) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new("7")
+                                )
+                            )
+                            .width(Length::Fill)
+                            .height(Length::Fill)
+                            .on_press(Message::Input("7".to_string()))
+                        },
+                        (0,1) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new("8")
+                                )
+                            )
+                            .width(Length::Fill)
+                            .height(Length::Fill)
+                            .on_press(Message::Input("8".to_string()))
+                        },
+                        (0,2) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new("9")
+                                )
+                            )
+                            .width(Length::Fill)
+                            .height(Length::Fill)
+                            .on_press(Message::Input("9".to_string()))
+                        },
+                        (0,3) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new("/")
+                                )
+                            )
+                            .width(Length::Fill)
+                            .height(Length::Fill)
+                            .on_press(Message::Input("/".to_string()))
+                        },
+                        (1,0) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new("4")
+                                )
+                            )
+                            .width(Length::Fill)
+                            .height(Length::Fill)
+                            .on_press(Message::Input("4".to_string()))
+                        },
+                        (1,1) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new("5")
+                                )
+                            )
+                            .width(Length::Fill)
+                            .height(Length::Fill)
+                            .on_press(Message::Input("5".to_string()))
+                        },
+                        (1,2) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new("6")
+                                )
+                            )
+                            .width(Length::Fill)
+                            .height(Length::Fill)
+                            .on_press(Message::Input("6".to_string()))
+                        },
+                        (1,3) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new("*")
+                                )
+                            )
+                            .width(Length::Fill)
+                            .height(Length::Fill)
+                            .on_press(Message::Input("*".to_string()))
+                        },
+                        (2,0) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new("1")
+                                )
+                            )
+                            .width(Length::Fill)
+                            .height(Length::Fill)
+                            .on_press(Message::Input("1".to_string()))
+                        },
+                        (2,1) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new("2")
+                                )
+                            )
+                            .width(Length::Fill)
+                            .height(Length::Fill)
+                            .on_press(Message::Input("2".to_string()))
+                        },
+                        (2,2) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new("3")
+                                )
+                            )
+                            .width(Length::Fill)
+                            .height(Length::Fill)
+                            .on_press(Message::Input("3".to_string()))
+                        },
+                        (2,3) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new("-")
+                                )
+                            )
+                            .width(Length::Fill)
+                            .height(Length::Fill)
+                            .on_press(Message::Input("-".to_string()))
+                        },
+                        (3,0) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new("0")
+                                )
+                            )
+                            .width(Length::Fill)
+                            .height(Length::Fill)
+                            .on_press(Message::Input("0".to_string()))
+                        },
+                        (3,1) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new(".")
+                                )
+                            )
+                            .width(Length::Fill)
+                            .height(Length::Fill)
+                            .on_press(Message::Input(".".to_string()))
+                        },
+                        (3,2) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new("=")
+                                )
+                            )
+                            .width(Length::Fill)
+                            .height(Length::Fill)
+                            .on_press(Message::Evaluate)
+                        },
+                        (3,3) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new("+")
+                                )
+                            )
+                            .width(Length::Fill)
+                            .height(Length::Fill)
+                            .on_press(Message::Input("+".to_string()))
+                        },
+                        (_,_) => {
+                            Button::new(
+                                Container::new(
+                                    Text::new("A")
+                                )
+                            )
+                        }
+                    }
+                )
+
+            }
+            grid = grid.push(row);
+        }
+
+        Column::new()
+            .push(text_box)
+            .push(grid)
+            //.width(Length::Fill)
+            
+        //.width(Length::Fill)
+        .padding(10)
+        .into()
+
+    }
 
 
-
-
-
-
-
-    // Grid::new()
-    //     .columns(4)
-    //     .push(
-    //         Button::new(
-    //             Text::new("1")
-    //                 .align_x(Alignment::Center)
-    //                 .align_y(Alignment::Center)
-    //         )
-    //         .width(BUTTON_SIZE)
-    //         .height(BUTTON_SIZE)
-    //     )
-    //     .push(
-    //         Button::new(
-    //             Text::new("+")
-    //                 .align_x(Alignment::Center)
-    //                 .align_y(Alignment::Center)
-    //         )
-    //         .width(BUTTON_SIZE)
-    //         .height(BUTTON_SIZE)
-    //     )
-    //     .push(
-    //         Button::new(
-    //             Text::new("2")
-    //                 .align_x(Alignment::Center)
-    //                 .align_y(Alignment::Center)
-    //         )
-    //         .width(BUTTON_SIZE)
-    //         .height(BUTTON_SIZE)
-    //     )
-    //     .push(
-    //         Button::new(
-    //             Text::new("=")
-    //                 .align_x(Alignment::Center)
-    //                 .align_y(Alignment::Center)
-    //         )
-    //         .width(BUTTON_SIZE)
-    //         .height(BUTTON_SIZE)
-    //     )
-        
-    // .spacing(10)
-    // .into()
-
-    //     .spacing(10)
-    // )
-    // .padding(10)
-    // .center_x(Fill)
-    // .center_y(Fill)
-    // .into()
 }
+
+
+
+
+
+
 
 
 
 pub fn main() -> iced::Result{
-    iced::run(update, view)
+   
+    iced::run(Calculator::update, Calculator::view)
+   
 }
