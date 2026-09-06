1b01eb46c410c5266fb9d9923b65207376b5c6ba
m0nkene <157753157+m0nkene@users.noreply.github.com>
2026-09-05 20:00:58 -0400

    tokenization complete
    Tokenizer finished, breaks string into a Token type with a value and a type enum. started work on the solver; going to implement basic, existing, rust features to perform the math

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,3 +1,42 @@
-pub fn tokenize(input: String){
-    println!("{}",input);
+#[derive(Debug)]
+pub enum TokenType{
+    Int,
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+#[derive(Debug)]
+pub struct Token{
+    token_type: TokenType,
+    token_value: String,
+}
+
+
+pub fn tokenize(input: &str) -> Vec<Token>{
+    
+    let mut tokens: Vec<Token> = vec![];
+    let mut current_num = String::new();
+
+    for c in input.chars().peekable(){
+
+        match c{
+            ' ' => continue,
+            '0'..='9' => tokens.push(create_token(TokenType::Int, c.to_string())),
+            '+' => tokens.push(create_token(TokenType::Add, c.to_string())),
+            '-' => tokens.push(create_token(TokenType::Sub, c.to_string())),
+            '*' => tokens.push(create_token(TokenType::Mul, c.to_string())),
+            '/' => tokens.push(create_token(TokenType::Div, c.to_string())),
+            _ => continue,
+        }
+    }
+    return tokens;
+}
+
+fn create_token(token_type: TokenType, token_value: String) -> Token {
+    Token{
+        token_type,
+        token_value,
+    }
 }
